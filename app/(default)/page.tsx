057974fd65321f057b226f177c910702b69d547a
Akash Singh <akashsunilsingh5555@gmail.com>
2024-02-24 00:02:05 +0530

    name

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,18 +1,18 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'Giveway | Social',
+  description: 'Giveway - Social',
 }
 
 import Hero from '@/components/hero'
 import Zigzag from '@/components/zigzag'
-import { Analytics } from "@vercel/analytics/react"
+// import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Zigzag />
-      <Analytics/>
+      {/* <Analytics/> */}
     </>
   )
 }
